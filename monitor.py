8dad9307db8cf3f129fd2585974db0795a083dbf
yyymac <yyytororo@gmail.com>
2026-04-06 22:16:34 +0900

    test

--- monitor.py
+++ monitor.py
@@ -12,6 +12,7 @@ TARGETS = {
     "かわさきテクノピア堀川町ハイツ": "https://www.ur-net.go.jp/chintai/sp/kanto/kanagawa/area/132.html",
     "アーベインビオ川崎": "https://www.ur-net.go.jp/chintai/sp/kanto/kanagawa/area/132.html",
     "サンスクエア川崎": "https://www.ur-net.go.jp/chintai/sp/kanto/kanagawa/area/131.html",
+    "西菅田": "https://www.ur-net.go.jp/chintai/kanto/kanagawa/area/102.html",
 }
 
 LAST_STATE_FILE = "last_state.json"
